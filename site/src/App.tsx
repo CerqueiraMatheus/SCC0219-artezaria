d915eccaa05ddd1e75b92a7907113fb496fe0417
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-06-16 19:54:21 -0300

    Add folder structure

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './presentation/components/Navigation/Footer';
+import Header from './presentation/components/Navigation/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +22,8 @@ function App() {
           Learn how to deliver a project in 3 days!
         </a>
       </header>
-    </div>
+      <Footer></Footer>
+    </div >
   );
 }
 

f107cc617f87e31939163e1183c36f60dc92bb9d
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-06-18 22:26:42 -0300

    Update app

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Footer from './presentation/components/Navigation/Footer';
-import Header from './presentation/components/Navigation/Header';
+import AppRouter from './presentation/routers/AppRouter';
+import {CartProvider} from './presentation/context/CartContext';
+import {UserProvider} from "./presentation/context/UserContext";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn how to deliver a project in 3 days!
-        </a>
-      </header>
-      <Footer></Footer>
-    </div >
-  );
+    return (
+        <div className="App">
+            <UserProvider>
+                <CartProvider>
+                    <AppRouter/>
+                </CartProvider>
+            </UserProvider>
+        </div>
+    );
 }
 
 export default App;

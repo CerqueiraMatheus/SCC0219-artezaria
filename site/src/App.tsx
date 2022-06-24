7fde7dad95d3925b1b0f65f243e8847f5d20b0b6
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-06-24 08:48:20 -0300

    Update site

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,13 +3,16 @@ import './App.css';
 import AppRouter from './presentation/routers/AppRouter';
 import {CartProvider} from './presentation/context/CartContext';
 import {UserProvider} from "./presentation/context/UserContext";
+import {ManagementProvider} from "./presentation/context/ManagementContext";
 
 function App() {
     return (
         <div className="App">
             <UserProvider>
                 <CartProvider>
-                    <AppRouter/>
+                    <ManagementProvider>
+                        <AppRouter/>
+                    </ManagementProvider>
                 </CartProvider>
             </UserProvider>
         </div>

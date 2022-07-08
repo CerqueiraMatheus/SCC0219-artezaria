5567768b6303d3732873358e4a17953c5c15502e
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-07-08 17:32:22 -0300

    Implements Sign Up

--- artezaria/server/src/Route/UserRoute.js
+++ artezaria/server/src/Route/UserRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('../Controller/UserController')
 
 // router.post('/:id', controller.getuserbyemail);
-// router.post('/', controller.post);
+router.post('/signup', controller.signUp);
 // router.put('/:id', controller.put);
 // router.delete('/:id', controller.delete);
 router.post("/signin", controller.signIn);

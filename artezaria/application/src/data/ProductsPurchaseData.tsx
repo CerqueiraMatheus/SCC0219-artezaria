96e189ed53378fc32450c107e1bb0d8e019a71b1
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-07-13 17:28:53 -0300

    Update

--- artezaria/application/src/data/ProductsPurchaseData.tsx
+++ artezaria/application/src/data/ProductsPurchaseData.tsx
@@ -2,8 +2,8 @@ import {PurchaseItem, PurchaseItemStatus} from "../domain/PurchaseItem";
 import {userClient} from "./UserData";
 import {productA} from "./ProductsData";
 
-const purchasesA = new PurchaseItem({id: 1, user: userClient, product: productA, status: PurchaseItemStatus.NOT_SENT});
-const purchasesB = new PurchaseItem({id: 2, user: userClient, product: productA, status: PurchaseItemStatus.SENT});
-const purchasesC = new PurchaseItem({id: 2, user: userClient, product: productA, status: PurchaseItemStatus.RECEIVED});
-
-export const PRODUCTS_PURCHASE = [purchasesA, purchasesB, purchasesC];
+// const purchasesA = new PurchaseItem({_id: "1", user: userClient, product: productA, status: PurchaseItemStatus.NOT_SENT});
+// const purchasesB = new PurchaseItem({_id: "2", user: userClient, product: productA, status: PurchaseItemStatus.SENT});
+// const purchasesC = new PurchaseItem({_id: "2", user: userClient, product: productA, status: PurchaseItemStatus.RECEIVED});
+//
+// export const PRODUCTS_PURCHASE = [purchasesA, purchasesB, purchasesC];

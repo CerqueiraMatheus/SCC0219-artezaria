fc6bb59e50cab42a0607d7117ac4be273d77c38b
Matheus Cerqueira <matheus.crqra@gmail.com>
2022-07-13 17:28:25 -0300

    Update server messages

--- artezaria/server/src/Util/Messages.js
+++ artezaria/server/src/Util/Messages.js
@@ -5,6 +5,11 @@ exports.SUCCESS = "Ok!";
 exports.SIGNIN_ERROR = "Usuário não encontrado";
 exports.SIGNUP_ERROR = "E-mail já existente";
 exports.USER_UPDATE_ERROR = "Usuário não encontrado";
+exports.USER_DELETE_PURCHASE_ERROR = "Usuário já tem compras cadastradas"
+exports.USER_DELETE_PRODUCT_ERROR = "Usuário já tem anúncios ativos"
 
 exports.PRODUCT_INSERT_ERROR = "Erro na inserção do produto";
 exports.PRODUCT_LIST_ERROR = "Erro na listagem de produtos";
+exports.PRODUCT_DELETE_ERROR = "Produto já foi comprado!"
+
+exports.PURCHASE_ERROR = "Erro na compra";
